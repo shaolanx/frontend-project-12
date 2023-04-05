5d201d14959e7f671fa70535a295966697f984f5
shaolanx <daggerx@mail.ru>
2023-04-05 19:57:51 +0300

    fix load main 2

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
@@ -19,11 +19,14 @@ const MainPage = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
+  const [loaded, setLoadStatus] = useState(true);
+
   useEffect(() => {
     axios.get(routes.dataPath(), { headers: getAuthHeader() })
       .then(({ data }) => {
         dispatch(channelsActions.setInitialChannels(data));
         dispatch(messagesActions.setInitialMessages(data));
+        setLoadStatus(false);
       })
       .catch((err) => {
         const trowAxiosError = () => {
@@ -39,13 +42,16 @@ const MainPage = () => {
       });
   });
 
-  return (
-    <div className="container h-100 my-4 overflow-hidden rounded shadow">
-      <div className="d-flex position-absolute justify-content-center">
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Загрузка...</span>
-        </div>
+  const dataUnLoaded = (
+    <div className="h-100 d-flex justify-content-center align-items-center">
+      <div role="status" className="spinner-border text-primary">
+        <span className="visually-hidden">Loading...</span>
       </div>
+    </div>
+  );
+
+  const dataLoaded = (
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
       <div className="row h-100 bg-white flex-md-row">
         <ChannelsContainer />
         <ChatContainer />
@@ -53,6 +59,8 @@ const MainPage = () => {
       </div>
     </div>
   );
+
+  return loaded === true ? dataUnLoaded : dataLoaded;
 };
 
 export default MainPage;
